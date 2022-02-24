bce3eb1246c5627154ce528f4cd92ce15b9a955d
cwparkexem <cwparkexem@gmail.com>
2022-02-24 17:30:24 +0900

    init commit

---
+++ src/test/java/com/exem/api/restquery/RestqueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exem.api.restquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestqueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
